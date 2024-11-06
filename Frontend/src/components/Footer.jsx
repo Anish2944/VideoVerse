f834f7fca4464d3a5b565d6b7f9d019744cb8572
ANISH KUSHWAHA <69107802+Anish2944@users.noreply.github.com>
2024-11-07 01:04:41 +0530

    footer name fixed

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="footer footer-center bg-base-300 text-base-content p-4">
         <aside>
           <p>
-            Copyright © {new Date().getFullYear()} - All right reserved by tubeMate
+            Copyright © {new Date().getFullYear()} - All right reserved by videoverse
             private Ltd
           </p>
         </aside>
