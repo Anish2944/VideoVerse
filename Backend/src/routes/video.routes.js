c0f9af25dc8307baa686f25cc949128cacb9b0ba
ANISH KUSHWAHA <69107802+Anish2944@users.noreply.github.com>
2025-07-13 19:02:21 +0530

    Update video.routes.js

--- Backend/src/routes/video.routes.js
+++ Backend/src/routes/video.routes.js
@@ -14,11 +14,11 @@ import { verifyJWT } from "../middlewares/auth.middleware.js"
 import { upload } from "../middlewares/multer.middleware.js"
 
 const router = Router();
-router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
+// router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
 router.route("/").get(getAllVideos);
 
-router.route("/upload-video").post(
+router.route("/upload-video").post(verifyJWT,
     upload.fields([
         {
             name: "videoFile",
@@ -32,18 +32,18 @@ router.route("/upload-video").post(
     publishAVideo
 );
 
-router.route('/views/:videoId').patch(incrementViews)
+router.route('/views/:videoId').patch( verifyJWT,incrementViews)
 
 router
     .route("/:videoId")
     .get(getVideoById)
-    .delete(deleteVideo)
-    .patch(upload.fields([{ name: "videoFile", maxCount: 1},{ name: "thumbnail", maxCount: 1}]), updateVideo);
+    .delete(verifyJWT,deleteVideo)
+    .patch(verifyJWT ,upload.fields([{ name: "videoFile", maxCount: 1},{ name: "thumbnail", maxCount: 1}]), updateVideo);
 
-router.route("/toggle/publish/:videoId").patch(togglePublishStatus);
+router.route("/toggle/publish/:videoId").patch(verifyJWT, togglePublishStatus);
 
-router.route('/uservideos/:userId').get(getAllUserVideos);
+router.route('/uservideos/:userId').get(verifyJWT, getAllUserVideos);
 
-router.route('/add-wh/:videoId').patch(setWatchHistory);
+router.route('/add-wh/:videoId').patch(verifyJWT,setWatchHistory);
 
 export default router
